43719980b26651ae5726b390134d6c441db5633f
chcorona <christopher.corona@ucdenver.edu>
2016-02-27 14:42:40 -0700

    linking up the definitions and declaration to the Cluster.cpp and Cluster.h

--- Cluster.cpp
+++ Cluster.cpp
@@ -8,49 +8,102 @@
 namespace Clustering
 {
 
+
+
+    Cluster::Cluster();
+
+    // The big three: cpy ctor, overloaded operator=, dtor
+    Cluster::Cluster(const Cluster &)
+    {
+
+    };
+    Cluster &operator=(const Cluster &)
+    {
+
+    };
+    Cluster::~Cluster()
+    {
+
+    };
+
+    // Getters/setters
+    int Cluster::getSize() const // TODO add to the requirements
+    {
+
+    };
+    // Set functions: They allow calling c1.add(c2.remove(p));
+    void Cluster::add(const Point &) // TODO add asc order to the requirements
+    {
+
+    };
+    const Point Cluster::&remove(const Point &)
+    {
+
+    };
+    bool Cluster::contains(const Point &)
+    {
+
+    };
+
+    // Overloaded operators
+
+    // Members: Subscript
+    const Point Cluster::&operator[](unsigned int index) const // notice: const
+    {
+
+    };
+    // Members: Compound assignment (Point argument)
+    Cluster &operator+=(const Point &);
+    Cluster &operator-=(const Point &);
+
+    // Members: Compound assignment (Cluster argument)
+    Cluster &operator+=(const Cluster &) // union
+    {
+
+    };
+    Cluster &operator-=(const Cluster &) // (asymmetric) difference
+    {
+
+    };
+
+    // Friends: IO
+    friend std::ostream &operator<<(std::ostream &, const Cluster &)
+    {
+
+    };
+    friend std::istream &operator>>(std::istream &, Cluster &)
+    {
+
+    };
+
+    // Friends: Comparison
+    friend bool operator==(const Cluster &, const Cluster &)
+    {
+
+    };
+    friend bool operator!=(const Cluster &, const Cluster &)
+    {
+
+    };
+
+    // Friends: Arithmetic (Cluster and Point)
+    friend const Cluster operator+(const Cluster &, const Point &)
+    {
+
+    };
+    friend const Cluster operator-(const Cluster &, const Point &)
+    {
+
+    };
+
+    // Friends: Arithmetic (two Clusters)
+    friend const Cluster operator+(const Cluster &, const Cluster &) // union
+    {
+
+    };
+    friend const Cluster operator-(const Cluster &, const Cluster &) // (asymmetric) difference
+    {
+
+    };
+
 };
-/*
-         Cluster();
-
-        // The big three: cpy ctor, overloaded operator=, dtor
-        Cluster(const Cluster &);
-        Cluster &operator=(const Cluster &);
-        ~Cluster();
-
-        // Getters/setters
-        int getSize() const; // TODO add to the requirements
-
-        // Set functions: They allow calling c1.add(c2.remove(p));
-        void add(const Point &); // TODO add asc order to the requirements
-        const Point &remove(const Point &);
-        bool contains(const Point &);
-
-        // Overloaded operators
-
-        // Members: Subscript
-        const Point &operator[](unsigned int index) const; // notice: const
-
-        // Members: Compound assignment (Point argument)
-        Cluster &operator+=(const Point &);
-        Cluster &operator-=(const Point &);
-
-        // Members: Compound assignment (Cluster argument)
-        Cluster &operator+=(const Cluster &); // union
-        Cluster &operator-=(const Cluster &); // (asymmetric) difference
-
-        // Friends: IO
-        friend std::ostream &operator<<(std::ostream &, const Cluster &);
-        friend std::istream &operator>>(std::istream &, Cluster &);
-
-        // Friends: Comparison
-        friend bool operator==(const Cluster &, const Cluster &);
-        friend bool operator!=(const Cluster &, const Cluster &);
-
-        // Friends: Arithmetic (Cluster and Point)
-        friend const Cluster operator+(const Cluster &, const Point &);
-        friend const Cluster operator-(const Cluster &, const Point &);
-
-        // Friends: Arithmetic (two Clusters)
-        friend const Cluster operator+(const Cluster &, const Cluster &); // union
-        friend const Cluster operator-(const Cluster &, const Cluster &); // (asymmetric) difference
- */
